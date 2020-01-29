e985e946cf413ff3a497b0d9a694525552bbe697
gkukanic <gkuka001@odu.edu>
2020-01-29 02:55:48 -0500

    Working on scheduling

--- calExport.py
+++ calExport.py
@@ -1,14 +1,14 @@
 from ics import Calendar, Event
 from Calculations import *
-import datetime
-
+from datetime import datetime
+from pytz import timezone
 class cal:
      def createCal(self, name):
           c = Calendar()
           e = Event()
           e.name = name
-          e.begin = '2020-01-27 22:30:00'
-          e.end = '2020-01-28 23:59:00'
+          e.begin = '2020-01-30 22:30:00'
+          e.end = '2020-02-21 23:59:00'
           c.events.add(e)
           c.events
           with open('DD_Calendar.ics', 'w') as my_file:
@@ -17,16 +17,17 @@ class cal:
      def scheduler(self):
           c = Calendar()
           e = Event()
-          begin = "21:30:00"
           for x in range(Calc.dates.__len__()):
-               datetime.date = Calc.dates[x]
-               datetime.datetime.combine(Calc.dates[x],) Calc.dates[x]
-               print("test")
-               e.begin = begin + beg
-               print(e.begin)
-               e.name = Calc.dates[x]
-               e.duration = datetime.timedelta(hours=5)
+               date = Calc.dates[x]
+               time = " 21:30:00"
+               date_time = str(date) + time
+               datetime_obj_naive = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+               datetime_obj_eastern = timezone('US/Eastern').localize(datetime_obj_naive)
+               print(datetime_obj_eastern.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+               e.begin = datetime_obj_eastern
+               e.name = "Designated Drivers"
+               e.duration = timedelta(hours=5)
                c.events.add(e)
                c.events
-               with open('DD_Calendar.ics', 'w') as my_file:
-                    my_file.writelines(c)
+          with open('DD_Calendar.ics', 'w') as my_file:
+               my_file.writelines(c)
